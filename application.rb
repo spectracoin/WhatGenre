68e3b4e3d1e617de82fe861699f5b88e2325c137
khendrikse <hendrikse.k@gmail.com>
2017-09-17 15:44:54 +0200

    Try to fix error 1

--- application.rb
+++ application.rb
@@ -6,15 +6,14 @@ RSpotify.authenticate(ENV['clientid'], ENV['clientsecret'])
 $input = ""
 
 get "/" do
-   if $input.empty?
-     $image = "image1.jpg"
-   end
+  #  if $input.empty?
+  #    $image = "image1.jpg"
+  #  end
   backgroundcolor
   erb :index
 end
 
 def backgroundcolor
-  puts "hellooo"
   @colors = [{"#E50914" => "#282581"}, {"#FF0000" => "#0A0D44"}, {"#00FF8F" => "#0A00A4"}, {"#FFF300" => "#E80000"}, {"#00E8C5" => "#5A009C"}, {"#FF9E00" => "#5A009C"}, {"#FFEC00" => "#FF00A6"}, {"#51FF00" => "#7400BF"}]
   @sample = @colors.sample
   @color1, @color2 = @sample.first

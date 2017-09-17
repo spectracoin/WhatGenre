1d4deef6e1047bfeed92e68f679a5eea420f318a
khendrikse <hendrikse.k@gmail.com>
2017-09-17 15:34:00 +0200

    First commit

---
+++ application.rb
@@ -0,0 +1,34 @@
+require "sinatra"
+require 'rspotify'
+require 'sinatra/static_assets'
+
+RSpotify.authenticate(ENV['clientid'], ENV['clientsecret'])
+$input = ""
+
+get "/" do
+   if $input.empty?
+     $image = "image1.jpg"
+   end
+  backgroundcolor
+  erb :index
+end
+
+def backgroundcolor
+  puts "hellooo"
+  @colors = [{"#E50914" => "#282581"}, {"#FF0000" => "#0A0D44"}, {"#00FF8F" => "#0A00A4"}, {"#FFF300" => "#E80000"}, {"#00E8C5" => "#5A009C"}, {"#FF9E00" => "#5A009C"}, {"#FFEC00" => "#FF00A6"}, {"#51FF00" => "#7400BF"}]
+  @sample = @colors.sample
+  @color1, @color2 = @sample.first
+end
+
+post "/" do
+  $input = params["artist"]
+  if $input.empty?
+    redirect back
+  else
+    $search = RSpotify::Artist.search($input)
+    $artist = $search.first
+    $genres = $artist.genres
+    $image = $artist.images.first["url"]
+    redirect back
+  end
+end

e8990800b27bc7dac4afed0abd45508375549c2d
khendrikse <hendrikse.k@gmail.com>
2017-10-10 23:00:14 +0200

    Cleaned app, now have to clean views+css

--- application.rb
+++ application.rb
@@ -2,7 +2,6 @@ require "sinatra"
 require 'rspotify'
 require 'sinatra/static_assets'
 
-# Authentification for Spotify API.
 RSpotify.authenticate(ENV['clientid'], ENV['clientsecret'])
 
 get "/" do
@@ -14,7 +13,6 @@ post "/genres" do
   if !params["artist"].empty?
     getgenres params["artist"]
     if @check == false
-      @tryagain = true
       backgroundcolor
       erb :index
     else
@@ -35,23 +33,11 @@ post "/related" do
 end
 
 def getgenres(artist = "")
-  @image = "image1.jpg"
-  @artistInput = artist
-  puts @artistInput
-  unless @artistInput.empty?
-    artistInfo = RSpotify::Artist.search(@artistInput).first
-    puts artistInfo
-    @id = artistInfo.id
-    puts @id
-    @check = false
-    unless artistInfo.nil? || artistInfo.genres.empty?
-      puts  "went through"
-      @check = true
-      @genres = artistInfo.genres
-        unless artistInfo.images.empty?
-          @image = artistInfo.images.first["url"]
-        end
-    end
+  @check = false
+  @artist = RSpotify::Artist.search(artist).first
+  unless @artist.nil? || @artist.genres.empty?
+    @check = true
+    getimage
   end
 end
 
@@ -65,8 +51,16 @@ def relatedartists(genre = "")
   end
 end
 
+def getimage
+  if @artist.images.empty?
+    @image = "image1.jpg"
+  else
+    @image = @artist.images.first["url"]
+  end
+end
+
 def backgroundcolor
   colors = {
     "#E50914" => "#282581", "#FF0000" => "#0A0D44", "#00FF8F" => "#0A00A4", "#FFF300" => "#E80000", "#00E8C5" => "#5A009C", "#FF9E00" => "#5A009C", "#FFEC00" => "#FF00A6", "#51FF00" => "#7400BF"}
-  @color1, @color2  = colors.to_a.sample
-end
+    @color1, @color2  = colors.to_a.sample
+  end

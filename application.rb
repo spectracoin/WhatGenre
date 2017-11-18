8954dc8e947553d014113568ec882487734634eb
khendrikse <hendrikse.k@gmail.com>
2017-11-18 15:49:58 +0100

    can now authorize and get user data

--- application.rb
+++ application.rb
@@ -1,9 +1,47 @@
 require "sinatra"
 require 'rspotify'
 require 'sinatra/static_assets'
-
+require "sinatra/reloader" if development?
+require 'uri'
+require 'net/http'
+require 'json'
 
 RSpotify.authenticate(ENV['clientid'], ENV['clientsecret'])
+
+# asks user to authenticate then redirects back to /callback
+get "/auth" do
+  redirect "https://accounts.spotify.com/authorize/?client_id=#{ENV['clientid']}&response_type=code&redirect_uri=http%3A%2F%2Flocalhost:4567%2Fcallback&scope=playlist-modify-public%20playlist-modify-private&state=34fFs29kd09"
+end
+
+# gets /callback
+get "/callback" do
+  # takes code from url params
+  code = params["code"]
+
+  # requests token from spotify using the users code => have to clean!
+  uri = URI.parse("https://accounts.spotify.com/api/token")
+
+  response = Net::HTTP.post_form(uri, {'grant_type' => 'authorization_code', 'code' => code, 'redirect_uri' => 'http://localhost:4567/callback', 'client_id' => ENV['clientid'], 'client_secret' => ENV['clientsecret'] })
+
+  # converts JSON into ruby hash
+  data = JSON.parse(response.read_body)
+
+  # gets user's data
+  url = URI("https://api.spotify.com/v1/me")
+
+  http = Net::HTTP.new(uri.host, uri.port)
+  http.use_ssl = true #this helps with the https
+
+  request = Net::HTTP::Get.new(url)
+  request["authorization"] = "Bearer #{data["access_token"]}"
+  response = http.request(request)
+  puts response.read_body
+end
+
+
+def playlist
+  uri = URI.parse("https://accounts.spotify.com/api/token")
+end
 
 get "/" do
   backgroundcolor
